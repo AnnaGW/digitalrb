38494ba097aac298b2bf33d655a6c131c99ea5f0
AnyaGW <a.bogoslovskaja@yandex.ru>
2026-03-18 01:18:24 +0500

    Улучшены стили для страницы 404

--- src/_/error404/index.tsx
+++ src/_/error404/index.tsx
@@ -1,10 +1,17 @@
+import s from './styles.module.css';
+
 function Error404() {
-  return (
-    <>
-      <h1>Страница 404</h1>
-      <h2>Ничего не нашлось</h2>
-    </>
-  );
+	return (
+		<div className={s.wrap404}>
+			<div className={s.innerWrap}>
+				<h1 className={s.wrap404Headline}>Ошибка 404</h1>
+				<h2 className={s.wrap404Text}>К сожалению, такой страницы нет</h2>
+				<a href="/" className={s.wrap404Link}>
+					На главную &#8594;
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default Error404;
